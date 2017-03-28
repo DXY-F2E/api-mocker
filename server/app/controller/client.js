b1eebe97dcf66df9f4ef321df918a5a8b22089d0
wei Quan <weiquan@weideMacBook-Pro.local>
2017-03-28 11:23:41 +0800

    chore: dsl-core close debug mode

--- server/app/controller/client.js
+++ server/app/controller/client.js
@@ -7,14 +7,14 @@ module.exports = app => {
             const { id }= this.ctx.params
             const document = yield app.model.api.findOne({url: `/client/${id}`, "options.method": /get/i}).exec()
             if (document) {
-                this.ctx.body = renderer(this.ctx.request.query, true)(document.dsl)
+                this.ctx.body = renderer(this.ctx.request.query)(document.dsl)
             }
         }
         // post /
         * create () {
             const document = yield app.model.api.findOne({url: this.ctx.request.url, "options.method": /post/i}).exec()
             if (document) {
-                this.ctx.body = renderer(R.merge(this.ctx.request.body, this.ctx.request.params), true)(document.dsl)
+                this.ctx.body = renderer(R.merge(this.ctx.request.body, this.ctx.request.params))(document.dsl)
             }
         }
     }
