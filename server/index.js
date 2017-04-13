fd372890ebbdcdb8c207a17317d411cd5bb61e55
quanwei <weiquan@weideMacBook-Pro.local>
2017-04-13 15:38:24 +0800

    feat: https support

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-
-
 // npm run dev DO NOT read this file
-
 require('egg').startCluster({
+    https: true,
+    key: '../domain.key',
+    cert: '../domain.cert',
     baseDir: __dirname,
     port: process.env.PORT || 7001 // default to 7001
 });
