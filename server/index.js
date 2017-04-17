c5ee1378c60e402376e3a75e94b870a7b9de7543
quanwei <weiquan@weideMacBook-Pro.local>
2017-04-17 18:40:18 +0800

    fix: https修复

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 // npm run dev DO NOT read this file
 require('egg').startCluster({
-    https: false,
-    key: '../domain.key',
-    cert: '../domain.cert',
+    https: true,
+    key: '../192.168.200.206.key',
+    cert: '../192.168.200.206.crt',
     baseDir: __dirname,
     port: process.env.PORT || 7001 // default to 7001
 });
