7288e3c788e0c7228c3d101ecd0965d8622a9e5f
quanwei <weiquan@weideMacBook-Pro.local>
2017-04-21 15:58:15 +0800

    add unit test & fix up

--- server/app/controller/api.js
+++ server/app/controller/api.js
@@ -1,9 +1,8 @@
 const R = require('ramda')
 const util = require('../util.js')
 const assert = require('http-assert')
-const sleep = (ms) => {
-  return cb => setTimeout(cb, ms)
-}
+const mongoose = require('mongoose')
+
 module.exports = app => {
     class ApiController extends app.Controller {
         * getAll () {
@@ -23,7 +22,6 @@ module.exports = app => {
             }
             if (groupId)
                 condition.group = groupId
-            // yield sleep(3000)
             const resources = yield app.model.api
                                        .find(condition)
                                        .sort({modifiedTime: -1, createTime: -1})
@@ -31,17 +29,18 @@ module.exports = app => {
                                        .limit(limit)
                                        .exec()
             const count = yield app.model.api.find(condition).count().exec()
-            this.ctx.body = { resources , pages: { limit, page, count}}
+            this.ctx.logger.info('getALl', this.ctx.query)
+            this.ctx.body = { resources, pages: { limit, page, count}}
             this.ctx.status = 200
-            this.ctx.logger.info('getALl', this.ctx.query);
         }
         * modifyApi () {
             const { groupId, apiId } = this.ctx.params
             const { body } = this.ctx.request
-            assert(groupId, 403, 'invalid groupId')
-            assert(apiId, 403, 'invalid apiId')
 
-            delete body._id;
+            assert(mongoose.Types.ObjectId.isValid(groupId), 403, 'invalid groupId')
+            assert(mongoose.Types.ObjectId.isValid(apiId), 403, 'invalid apiId')
+
+            delete body._id
 
             const resources = yield app.model.api.findOneAndUpdate({
                 group: groupId,
@@ -50,27 +49,28 @@ module.exports = app => {
             //{new: true} 使结果能返回更新后的数据
             yield app.model.group.update({_id: groupId}, {modifiedTime: Date.now()}, {new: true}).exec()
 
+            this.ctx.logger.info('modifyApi', body)
             this.ctx.body = { resources }
-            this.ctx.logger.info('modifyApi', body);
         }
         * getApi () {
             const { groupId, apiId } = this.ctx.params
-            assert(groupId, 403, 'invalid groupId')
-            assert(apiId, 403, 'invalid apiId')
+
+            assert(mongoose.Types.ObjectId.isValid(groupId), 403, 'invalid groupId')
+            assert(mongoose.Types.ObjectId.isValid(apiId), 403, 'invalid apiId')
 
             const resources = yield app.model.api
                                        .findOne({group: groupId, _id:apiId, isDeleted: false})
                                        .exec()
 
+            this.ctx.logger.info('getApi')
             this.ctx.body = { resources }
             this.ctx.status = 200
-            this.ctx.logger.info('getApi');
         }
         * createApi () {
             const { groupId } = this.ctx.params
             const { body } = this.ctx.request
 
-            assert(groupId, 403, 'invalie groupId')
+            assert(mongoose.Types.ObjectId.isValid(groupId), 403, 'invalie groupId')
             assert(body.name, 403, 'required name')
             assert(body.dsl, 403, 'required dsl')
 
@@ -81,17 +81,19 @@ module.exports = app => {
                 group: groupId,
                 url: nextUrl
             })).save()
+
             yield app.model.group.update({_id: groupId}, {modifiedTime: Date.now()}, {new: true}).exec()
 
+            this.ctx.logger.info('createApi', body)
             this.ctx.body = { resources }
             this.ctx.status = 200
-            this.ctx.logger.info('createApi', body);
         }
         * delete () {
             const { groupId } = this.ctx.params
+            const { groupId, apiId } = this.ctx.params
 
-            assert(groupId, 403, 'invalie groupId')
-            assert(groupId !== 'undefined', 403, 'groupId is undefined')
+            assert(mongoose.Types.ObjectId.isValid(groupId), 403, 'invalie groupId')
+            assert(mongoose.Types.ObjectId.isValid(apiId), 403, 'invalid apiId')
 
             yield app.model.api.update({
                 _id: this.ctx.params.apiId
@@ -99,8 +101,8 @@ module.exports = app => {
                 isDeleted: true
             })
             yield app.model.group.update({_id: groupId}, {modifiedTime: Date.now()}, {new: true}).exec()
+            this.ctx.logger.info('deleteApi')
             this.ctx.status = 204
-            this.logger.info('deleteApi');
         }
     }
     return ApiController
