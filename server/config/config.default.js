48278634aba9f454f24711dd7a0888b668b5531c
quanwei <weiquan@weideMacBook-Pro.local>
2017-04-21 16:01:29 +0800

    chore: config

--- server/config/config.default.js
+++ server/config/config.default.js
@@ -1,11 +1,11 @@
-
-
 module.exports = appInfo => {
     const config = {
+        mongoose: {
+            url: 'mongodb://localhost/api-mock'
+        }
     };
 
   // should change to your own
     config.keys = `${appInfo.name}_1490324849354_6879`;
-
     return config;
 };
