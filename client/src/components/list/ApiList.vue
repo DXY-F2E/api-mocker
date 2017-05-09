aa8afa452606c15de5b66fa4792e16e76ae2c610
zhangfx <xiang@zhangfengxiangdeMacBook-Pro.local>
2017-05-09 16:05:22 +0800

    优化创建接口

--- client/src/components/list/ApiList.vue
+++ client/src/components/list/ApiList.vue
@@ -4,6 +4,9 @@
             <li v-for="(api, idx) in apiList">
                 <api :data="api" :index="idx"></api>
             </li>
+            <li class="add-api" @click="createApi">
+                <el-card><i class="el-icon-plus"></i>创建接口</el-card>
+            </li>
             <li class="empty">暂无接口</li>
         </ul>
     </div>
@@ -17,8 +20,26 @@ export default {
     components: {
         Api
     },
+    methods: {
+        createApi() {
+            this.$router.push('/create');
+        }
+    },
     computed: mapState(['apiList', 'apiListLoading'])
 };
 </script>
 <style>
+.add-api .el-card .el-card__body {
+    height: 105px;
+    line-height: 75px;
+    text-align: center;
+    font-size: 16px;
+    color: #324057;
+}
+.add-api i {
+    font-size: 18px;
+    color: #99A9BF;
+    margin-right: 10px;
+    /*vertical-align: sub;*/
+}
 </style>
