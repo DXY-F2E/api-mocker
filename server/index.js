1307fae5aa0bedf2845265f63577b87fd1ad8e8c
zhangfx <xiang@zhangfengxiangdeMacBook-Pro.local>
2017-05-25 13:24:36 +0800

    https 链接走 ngnix转发

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 // npm run dev DO NOT read this file
-require('egg').startCluster({
-    https: true,
-    key: '../192.168.200.206.key',
-    cert: '../192.168.200.206.crt',
-    baseDir: __dirname,
-    port: process.env.PORT || 7001 // default to 7001
-});
+// require('egg').startCluster({
+//     https: true,
+//     key: '../192.168.200.206.key',
+//     cert: '../192.168.200.206.crt',
+//     baseDir: __dirname,
+//     port: process.env.PORT || 7001 // default to 7001
+// });
 
-// run两个程序，http 链接走 7002 端口
+// https 链接走 ngnix转发
 require('egg').startCluster({
     https: false,
     baseDir: __dirname,
-    port: 7002
+    port: process.env.PORT || 7001 // default to 7001
 });
