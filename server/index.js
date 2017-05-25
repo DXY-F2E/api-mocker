6ea9ff34b564e7b2ff9cfed184852ece39d485b4
zhangfx <xiang@zhangfengxiangdeMacBook-Pro.local>
2017-05-25 14:25:01 +0800

    尝试服务是https，ngnix把非http链接转发到https

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 // npm run dev DO NOT read this file
-// require('egg').startCluster({
-//     https: true,
-//     key: '../192.168.200.206.key',
-//     cert: '../192.168.200.206.crt',
-//     baseDir: __dirname,
-//     port: process.env.PORT || 7001 // default to 7001
-// });
-
-// https 链接走 ngnix转发
 require('egg').startCluster({
-    https: false,
+    https: true,
+    key: '../192.168.200.206.key',
+    cert: '../192.168.200.206.crt',
     baseDir: __dirname,
     port: process.env.PORT || 7001 // default to 7001
 });
+
+// https 链接走 ngnix转发
+// require('egg').startCluster({
+//     https: false,
+//     baseDir: __dirname,
+//     port: process.env.PORT || 7001 // default to 7001
+// });
