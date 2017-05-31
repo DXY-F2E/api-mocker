bb3185d0213b9a35f1bbda99f1ce14300d8baa98
zhangfx <xiang@zhangfengxiangdeMacBook-Pro.local>
2017-05-31 09:49:55 +0800

    底纹图片

--- client/src/components/auth/Index.vue
+++ client/src/components/auth/Index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="auth-page">
         <router-view></router-view>
     </div>
 </template>
@@ -8,4 +8,13 @@ export default {
 };
 </script>
 <style>
+.auth-page {
+    background-color: #f5f5f5;
+    background-image: url(~/assets/texture.png);
+    position: fixed;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+}
 </style>
