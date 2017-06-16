dd2686291bf896f335f5b7fa28da78ec3814debc
zhangfx <zhangfx@dxy.cn>
2017-06-16 14:16:17 +0800

    修改缓存方案

--- server/app/service/cache.js
+++ server/app/service/cache.js
@@ -1,30 +1,34 @@
-const LRU = require("lru-cache")
+const CACHE_NAME = 'ApiMocker'
 const DEFAULT_MAX_AGE = 1 * 1000 * 60
-const options = {
-  max: 500,
-  // length: function (n, key) { return n * 2 + key.length },
-  // dispose: function (key, n) { n.close() },
-  maxAge: DEFAULT_MAX_AGE
-}
-const Cache = LRU(options)
+
+const ShareCache = require('node-shared-cache')
+const Cache = new ShareCache.Cache(CACHE_NAME, 557056)
+ShareCache.release(CACHE_NAME)
 
 module.exports = app => {
     class CacheService extends app.Service {
-        create (key, value, maxAge = DEFAULT_MAX_AGE ) {
-          return Cache.set(key, value, maxAge)
+        create (key, value, maxAge = DEFAULT_MAX_AGE) {
+          return Cache.key = {
+            value,
+            expires: new Date(+ new Date() + maxAge).toString()
+          }
         }
-        verifyCodeCache (key, length, maxAge = DEFAULT_MAX_AGE ) {
+        verifyCodeCache (key, length, maxAge = DEFAULT_MAX_AGE) {
           const code = Array.from({length}, () => Math.ceil(Math.random() * 9)).join('')
-          return this.create(key, code, maxAge) && code
+          return this.create(key, code) && code
+        }
+        isExpires(key) {
+          return new Date(Cache.key.expires) < new Date()
         }
         get (key) {
-          return Cache.peek(key)
+          return this.has(key) ? Cache.key.value : undefined
         }
         del (key) {
-          return Cache.del(key)
+          return delete Cache.key
         }
         has (key) {
-          return Cache.has(key)
+          const cache = Cache.key
+          return cache !== undefined && new Date(cache.expires) >= new Date()
         }
     }
     return CacheService;
