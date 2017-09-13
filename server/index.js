a7b03777483f9facb838fb3f32b5c2edbb3205c4
zhangfx <zhangfx@dxy.cn>
2017-09-13 16:37:35 +0800

    移动证书文件目录

--- server/index.js
+++ server/index.js
@@ -1,9 +1,7 @@
-const path = require('path')
-
 require('egg').startCluster({
   https: true,
-  key: path.join(__dirname, '../192.168.200.206.key'),
-  cert: path.join(__dirname, '../192.168.200.206.crt'),
+  key: './certs/192.168.200.206.key',
+  cert: './certs/192.168.200.206.crt',
   baseDir: __dirname,
   workers: 4,
   port: process.env.PORT || 7001 // default to 7001
