385f47feedd671cf3870ffd985fede557bc0376f
zhangfx <zhangfx@dxy.cn>
2017-11-14 20:51:58 +0800

    set cert path

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
+const path = require('path')
 process.env.NODE_ENV = 'production'
 
 require('egg').startCluster({
   // 若需要https服务，请取消注释，并配置好证书文件
   https: true,
-  key: './certs/192.168.200.206.key',
-  cert: './certs/192.168.200.206.crt',
+  key: path.join(__dirname, './certs/192.168.200.206.key'),
+  cert: path.join(__dirname, './certs/192.168.200.206.crt'),
   baseDir: __dirname,
   workers: 4,
   port: process.env.PORT || 7001 // default to 7001
