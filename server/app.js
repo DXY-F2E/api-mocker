62ee2a10c4a02040e4c0ee11ee9060054d199bc2
wuomzfx <75851654@qq.com>
2017-12-01 14:55:03 +0800

    remove useless code

--- server/app.js
+++ server/app.js
@@ -1,31 +1,4 @@
 module.exports = app => {
-  class AbstractController extends app.Controller {
-    success (data) {
-      this.ctx.status = 200
-      this.ctx.body = data
-    }
-    fail (msg) {
-      // 弃用
-      this.ctx.body = {
-        success: false,
-        msg
-      }
-    }
-    error (data, code = 403) {
-      if (typeof data === 'string') {
-        this.ctx.throw(code, data)
-      } else {
-        this.ctx.throw(data.code, data.msg)
-      }
-    }
-    notFound (msg) {
-      msg = msg || 'not found'
-      this.ctx.throw(404, msg)
-    }
-  }
-
-  app.Controller = AbstractController
-
   // 数字校验-允许提交字符串格式的数字
   app.validator.addRule('unstrict_number', (rule, value) => {
     if (value && !isNaN(value)) {
