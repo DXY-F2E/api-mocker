969c86576b951a31e7e6ff313d51bc19f54776f0
majinbo <majinbo@dxy.cn>
2019-01-16 16:57:03 +0800

    fix: 将邮箱和 md5Key 从示例中移除

--- server/config/core.default.js
+++ server/config/core.default.js
@@ -5,7 +5,7 @@ module.exports = config => ({
     url: 'mongodb://127.0.0.1/api-mock'
   },
   // 密码加密的key
-  md5Key: '52851cb05258c8d98da1672d95729e53',
+  md5Key: 'xxx',
   // 发送邮件配置
   transporter: {
     appName: 'Api Mocker',
@@ -13,8 +13,8 @@ module.exports = config => ({
     secure: true,
     port: 465,
     auth: {
-      user: 'dxyf2e@dxy.cn',
-      pass: 'Dxyf2e222'
+      user: 'xxx@dxy.cn',
+      pass: 'xxx'
     }
   }
 })
