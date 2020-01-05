d895f6f63b73c7ac205170d4ed63bcabcea403c1
liujipeng <liujipeng@dxy.cn>
2020-01-05 15:59:55 +0800

    feat: 去掉dxylogin相关信息

--- server/config/config.local.js
+++ server/config/config.local.js
@@ -1,12 +1,5 @@
 module.exports = appInfo => {
   return {
-    // dxy login
-    dxyLogin: {
-      url: 'http://up2u.dxy.net/user/info',
-      system: 'mocker',
-      appId: '1310386539',
-      appSignKey: 'qCDyQLbQzNWTFg3FRXtM1MIuXWz1TCxXcTT5xcqouzkTsqVBpTJyqW7cJQVpXyazmuuHtiNmCbMXBfxrQojbUZOHMIz3srrnYzoDyHDIpdpUkw7CeoiO7M1XKpfJCXTp'
-    },
     clientRoot: 'http://localhost:8080'
   }
 }
